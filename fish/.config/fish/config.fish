b3e0d395632ee58ecde590c99c88f4aab901e3c8
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-11-20 02:26:42 +0000

    added cli pomodoro timer by bashbunni

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
+# startup commands
 if status is-interactive
     fortune | cowsay -f stegosaurus
-    # neofetch
 end
 
 starship init fish | source
@@ -19,6 +19,7 @@ if not string match -q -- /usr/bin/go $PATH
     set -gx PATH /usr/bin/go $PATH
 end
 
+# go variables
 set -gx GOPATH $HOME/go
 set -gx GOROOT /usr/lib/go
 set -Ux fish_user_paths $HOME/go/bin $fish_user_paths
@@ -28,4 +29,18 @@ set -gx PNPM_HOME "/home/vieira/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
+
+# bashbunni pomodoro cli timmer
+# Define pomodoro options as key-value pairs
+set -l pomo_options_work 45
+set -l pomo_options_break 10
+
+function pomodoro 
+  echo $argv[1] | lolcat
+  timer "$argv[2]"m 
+  ep "'$argv[1]' session done"
+end
+
+alias po="pomodoro"
+alias wo="pomodoro work 45"
+alias br="pomodoro break 10"
