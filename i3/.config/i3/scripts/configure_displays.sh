55578ae219da7e3a4d3e2b2de96085cfdf966c7e
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-11-20 01:44:48 +0000

    fixed configure_displays bash scripts + removed boilerplate code

--- i3/.config/i3/scripts/configure_displays.sh
+++ i3/.config/i3/scripts/configure_displays.sh
@@ -2,11 +2,11 @@
 
 HDMI_STATUS=$(xrandr | grep "HDMI-1 connected")
 
-PRIMARY="HDMI-1"  
-SECONDARY="eDP-1"  
+PRIMARY="HDMI-1"
+SECONDARY="eDP-1"
 
 if [ -n "$HDMI_STATUS" ]; then
-    xrandr --output HDMI-1 --primary --auto --output eDP-1 -off
+    xrandr --output HDMI-1 --primary --auto --right-of eDP-1 --output eDP-1 --auto
 else
     xrandr --output eDP-1 --primary --auto --output HDMI-1 --off
 fi
--- nvim/.config/nvim/lua/vieira/plugins/mason.lua
+++ nvim/.config/nvim/lua/vieira/plugins/mason.lua
@@ -1,47 +1,49 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"emmet_ls",
-				"pyright",
-				"gopls",
-			},
-		})
+        mason_lspconfig.setup({
+            -- list of servers for mason to install
+            ensure_installed = {
+                "html",
+                "cssls",
+                "tailwindcss",
+                "lua_ls",
+                "emmet_ls",
+                "pyright",
+                "gopls",
+                "kotlin-language-server",
+            },
+        })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint",
-				"eslint_d",
-			},
-		})
-	end,
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua",   -- lua formatter
+                "isort",    -- python formatter
+                "black",    -- python formatter
+                "pylint",
+                "eslint_d",
+                "ktlint" -- kotlin linter
+            },
+        })
+    end,
 }
