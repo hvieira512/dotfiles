ceaadaa5900c93d8ae103940cc7c04303ab8e518
Hugo Vieira <hugo.vieira2002@hotmail.com>
2024-11-30 21:00:08 +0000

    removed sessions in neovim + added lazygit

--- nvim/.config/nvim/lua/vieira/plugins/mini.lua
+++ nvim/.config/nvim/lua/vieira/plugins/mini.lua
@@ -2,8 +2,8 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-        local mini = require("mini.ai")
-
-        mini.setup({})
+        require("mini.ai").setup()
+        require("mini.notify").setup()
+        require("mini.icons").setup()
     end
 }
