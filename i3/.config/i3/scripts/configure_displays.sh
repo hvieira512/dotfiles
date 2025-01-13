b39c469d0034e69e21770cf122d787580afb0e05
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-01-13 16:06:16 +0000

    feat: i3 stuff

--- i3/.config/i3/scripts/configure_displays.sh
+++ i3/.config/i3/scripts/configure_displays.sh
@@ -6,7 +6,8 @@ PRIMARY="HDMI-1"
 SECONDARY="eDP-1"
 
 if [ -n "$HDMI_STATUS" ]; then
-    xrandr --output HDMI-1 --primary --auto --right-of eDP-1 --output eDP-1 --auto
+    # xrandr --output HDMI-1 --primary --auto --right-of eDP-1 --output eDP-1 --auto
+    xrandr --output HDMI-1 --primary --auto --output eDP-1 --off
 else
     xrandr --output eDP-1 --primary --auto --output HDMI-1 --off
 fi
