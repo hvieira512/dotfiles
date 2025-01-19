248c886d5c2f8db8bfbf012559f9bd305929da38
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-01-19 16:43:51 +0000

    nvim: removed auto completion when in command move

--- nvim/.config/nvim/lua/vieira/plugins/blink.lua
+++ nvim/.config/nvim/lua/vieira/plugins/blink.lua
@@ -9,6 +9,11 @@ return {
             ["<C-j>"] = { 'select_next', 'fallback' },
         },
 
+        -- Do not complete when in command mode
+        completion = {
+            menu = { auto_show = function(ctx) return ctx.mode ~= 'cmdline' end }
+        },
+
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
